3910481c6878e9ff81b8ee755d45a9e05d474fcb
muhmmad Nabh <muhammadnabih2020@gmail.com>
2023-10-02 19:54:09 +0300

    make counter in our awesome stats section

--- main.js
+++ main.js
@@ -1,18 +1,60 @@
-// استخدم const لتعريف المتغيرات إذا كانت لا تتغير
+// Selectors for Progress Bars
 const ourSkillSection = document.querySelector(".our-skills");
 const progressBars = document.querySelectorAll(".our-skills .progress span");
 
-console.log(ourSkillSection);
-
-// انفصل الوظيفة المعينة لمنسوب التمرير عن طريق إضافة مستمع الحدث باستخدام addEventListener
+// Function to Handle Progress Bars
 window.addEventListener("scroll", () => {
-  // افحص إذا كان الواجهة قد تجاوزت منطقة العنصر ".our-skills"
+  // Check if the user has scrolled to the "our-skills" section
   if (window.scrollY >= ourSkillSection.offsetTop) {
-    // استخدم forEach للتكرار على كل عنصر
+    // Loop through all progress bars
     progressBars.forEach((progressBar) => {
+      // Get the progress value from the "data-progress" attribute
       const progressValue = progressBar.dataset.progress;
-      // ضبط عرض الشريط باستخدام قيمة "data-progress"
+      // Set the width of the progress bar
       progressBar.style.width = progressValue;
     });
   }
 });
+
+// Selectors for Counters
+const allSpan = document.querySelectorAll(".number");
+const stats = document.querySelector(".stats");
+let start = false;
+
+// Function to Start Counters
+window.onscroll = () => {
+  // Check if the user has scrolled to the "stats" section
+  if (window.scrollY >= stats.offsetTop) {
+    // Check if the counters have already started
+    if (!start) {
+      // Loop through all number elements and start counting
+      allSpan.forEach((span) => {
+        counterFun(span);
+      });
+    }
+    // Set the counters as started
+    start = true;
+  }
+};
+
+// Function for Counting
+function counterFun(span) {
+  // Get the target goal from the "data-goal" attribute
+  let goal = span.dataset.goal;
+  // Initialize the current counter value
+  let current = 0;
+
+  // Use setInterval to gradually increase the counter value
+  let counter = setInterval(() => {
+    // Increment the current value
+    current++;
+    // Display the current value in the span element
+    span.textContent = current;
+
+    // Check if the counter has reached the goal
+    if (current === parseInt(goal)) {
+      // Stop the setInterval when the goal is reached
+      clearInterval(counter);
+    }
+  }, 6000 / parseInt(goal)); // Adjust the counter speed based on the goal
+}
